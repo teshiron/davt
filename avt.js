cf55f21394a2e13464290438c3826a7a96fa88e5
Ryan (darkwind) <darkwind@chaosfire.net>
2013-12-04 00:47:09 -0800

    add diff link and new warning options
    spam and promo

--- avt.js
+++ avt.js
@@ -4,6 +4,7 @@
  * Some of the functionality for the auto-warn feature is borrowed from Twinkle ([[WP:TW]])
  *
  * License: GFDL 1.3 or later*, CC-BY-SA-3.0*, or EPL 1.0 (your choice)
+ * <nowiki>
  */
 
 /* Configuration options:
@@ -528,12 +529,16 @@ AVT.diffDisplay = function(title, editor, timestamp, summary, matches, content, 
         newHTML += (isNewPage ? 'created' : 'performed') + ' by <span style="color: red"><b>an editor you rolled back ' + kvCount + ' time' + (kvCount > 1 ? 's.' : '.') + ' </b></span> ';
     }
 
+    //add a link to open the diff in a new page - this allows use of tools like TW for rollback if the edit is bad but doesn't fall within the types this tool covers
+    newHTML += '[<a href="https://en.wikipedia.org/w/index.php?oldid=' + revid + '&diff=prev" target="_blank">open diff</a>] ';
+
     //assemble line of links to rollback function, save it for later to add to the bottom
     //signature for rollback function is (editor, revid, divNumber, warnType)
     var rollbackfrag = "[<a href=\"javascript:AVT.rollback('" + editor + "', " + revid + ", " + AVT.count + ", "; //string is not closed to allow multiple warntypes
 
     var rollbackLine = "<br>" + rollbackfrag + "'none')\">rollback only</a>] Revert and warn: " + rollbackfrag + "'vandalism')\">vandalism</a>] " + rollbackfrag + "'test')\">test edit</a>] ";
     rollbackLine += rollbackfrag + "'delete')\">blanking</a>] " + rollbackfrag + "'joke')\">joke</a>] " + rollbackfrag + "'biog')\">BLP</a>] " + rollbackfrag + "'defam')\">defamation</a>] ";
+    rollbackLine += rollbackfrag + "'spam')\">spam links</a>] " + rollbackfrag + "'advert')\">advert/promo</a>] ";
 
     //add it to the HTML
     newHTML += rollbackLine + '<br>'; //and go to second line
@@ -762,6 +767,14 @@ AVT.rollback = function(editor, revid, divNumber, warnType) { //this function us
             break;
         case "defam":
             rollbackSummary += "[[WP:LIBEL|libelous or defamatory content]] ";
+            warningPriority = true;
+            break;
+        case "spam":
+            rollbackSummary += "[[WP:SPAM|spam link(s)]] ";
+            warningPriority = true;
+            break;
+        case "advert":
+            rollbackSummary += "promotional material ";
             warningPriority = true;
             break;
         default:
@@ -1063,3 +1076,4 @@ return (_1.length>0?_1[_2]:undefined);
 * http://www.opensource.org/licenses/mit-license.php
 */
 (function(f){var c={vertical:{x:false,y:true},horizontal:{x:true,y:false},both:{x:true,y:true},x:{x:true,y:false},y:{x:false,y:true}};var b={duration:"fast",direction:"both"};var e=/^(?:html)$/i;var g=function(k,j){j=j||(document.defaultView&&document.defaultView.getComputedStyle?document.defaultView.getComputedStyle(k,null):k.currentStyle);var i=document.defaultView&&document.defaultView.getComputedStyle?true:false;var h={top:(parseFloat(i?j.borderTopWidth:f.css(k,"borderTopWidth"))||0),left:(parseFloat(i?j.borderLeftWidth:f.css(k,"borderLeftWidth"))||0),bottom:(parseFloat(i?j.borderBottomWidth:f.css(k,"borderBottomWidth"))||0),right:(parseFloat(i?j.borderRightWidth:f.css(k,"borderRightWidth"))||0)};return{top:h.top,left:h.left,bottom:h.bottom,right:h.right,vertical:h.top+h.bottom,horizontal:h.left+h.right}};var d=function(h){var j=f(window);var i=e.test(h[0].nodeName);return{border:i?{top:0,left:0,bottom:0,right:0}:g(h[0]),scroll:{top:(i?j:h).scrollTop(),left:(i?j:h).scrollLeft()},scrollbar:{right:i?0:h.innerWidth()-h[0].clientWidth,bottom:i?0:h.innerHeight()-h[0].clientHeight},rect:(function(){var k=h[0].getBoundingClientRect();return{top:i?0:k.top,left:i?0:k.left,bottom:i?h[0].clientHeight:k.bottom,right:i?h[0].clientWidth:k.right}})()}};f.fn.extend({scrollintoview:function(j){j=f.extend({},b,j);j.direction=c[typeof(j.direction)==="string"&&j.direction.toLowerCase()]||c.both;var n="";if(j.direction.x===true){n="horizontal"}if(j.direction.y===true){n=n?"both":"vertical"}var l=this.eq(0);var i=l.closest(":scrollable("+n+")");if(i.length>0){i=i.eq(0);var m={e:d(l),s:d(i)};var h={top:m.e.rect.top-(m.s.rect.top+m.s.border.top),bottom:m.s.rect.bottom-m.s.border.bottom-m.s.scrollbar.bottom-m.e.rect.bottom,left:m.e.rect.left-(m.s.rect.left+m.s.border.left),right:m.s.rect.right-m.s.border.right-m.s.scrollbar.right-m.e.rect.right};var k={};if(j.direction.y===true){if(h.top<0){k.scrollTop=m.s.scroll.top+h.top}else{if(h.top>0&&h.bottom<0){k.scrollTop=m.s.scroll.top+Math.min(h.top,-h.bottom)}}}if(j.direction.x===true){if(h.left<0){k.scrollLeft=m.s.scroll.left+h.left}else{if(h.left>0&&h.right<0){k.scrollLeft=m.s.scroll.left+Math.min(h.left,-h.right)}}}if(!f.isEmptyObject(k)){if(e.test(i[0].nodeName)){i=f("html,body")}i.animate(k,j.duration).eq(0).queue(function(o){f.isFunction(j.complete)&&j.complete.call(i[0]);o()})}else{f.isFunction(j.complete)&&j.complete.call(i[0])}}return this}});var a={auto:true,scroll:true,visible:false,hidden:false};f.extend(f.expr[":"],{scrollable:function(k,i,n,h){var m=c[typeof(n[3])==="string"&&n[3].toLowerCase()]||c.both;var l=(document.defaultView&&document.defaultView.getComputedStyle?document.defaultView.getComputedStyle(k,null):k.currentStyle);var o={x:a[l.overflowX.toLowerCase()]||false,y:a[l.overflowY.toLowerCase()]||false,isRoot:e.test(k.nodeName)};if(!o.x&&!o.y&&!o.isRoot){return false}var j={height:{scroll:k.scrollHeight,client:k.clientHeight},width:{scroll:k.scrollWidth,client:k.clientWidth},scrollableX:function(){return(o.x||o.isRoot)&&this.width.scroll>this.width.client},scrollableY:function(){return(o.y||o.isRoot)&&this.height.scroll>this.height.client}};return m.y&&j.scrollableY()||m.x&&j.scrollableX()}})})(jQuery);
+//</nowiki>
